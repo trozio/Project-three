8fa5fc615c7317d49c55cbd0be91a3fd28a9fa72
trozio <daniel.l.adelman@gmail.com>
2019-04-11 18:43:57 -0400

    routes

--- server.js
+++ server.js
@@ -2,10 +2,12 @@ let express = require("express");
 let app = express();
 let path = require("path");
 let mongoose = require("mongoose");
+const routes = require("./routes");
+
 
 let PORT = process.env.PORT || 3000;
 
-
+app.use(routes);
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
