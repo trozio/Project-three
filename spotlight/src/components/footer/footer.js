eda81799d4e53b8e33bee372b43007b168c8d293
trozio <daniel.l.adelman@gmail.com>
2019-04-12 18:00:26 -0400

    routes

--- spotlight/src/components/footer/footer.js
+++ spotlight/src/components/footer/footer.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'react-bootstrap';
-import '../Layouts/Footer.css';
+import './Footer.css';
 export default class Footer extends Component {
    render() {
         return (
