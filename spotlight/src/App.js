ebba35e1a219a5aa15ba8da110a7da87c9bf2ede
helmi <helmitira@yahoo.com>
2019-04-15 12:48:58 -0400

    adding routes

--- spotlight/src/App.js
+++ spotlight/src/App.js
@@ -1,4 +1,5 @@
 import React, { Component} from 'react';
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import Header from './components/Layouts/Header';
 import Footer from "./components/Layouts/Footer";
 import Home from "./components/Layouts/Home";
@@ -6,13 +7,15 @@ import reviews from "./components/Layouts/reviews";
 class App extends Component {
   render() {
     return (
+      <Router>
     <div>
       <Header/>
-      <Home/>
-      <reviews/>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/reviews" component={reviews} />
       <Footer/>
 
     </div>
+    </Router>
     );
   }
 }
