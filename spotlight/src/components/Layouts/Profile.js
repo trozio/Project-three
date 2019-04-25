593143bd54f5ed8e95343c3a14337a92351cd471
trozio <daniel.l.adelman@gmail.com>
2019-04-25 18:30:06 -0400

    spacing

--- spotlight/src/components/Layouts/Profile.js
+++ spotlight/src/components/Layouts/Profile.js
@@ -1,6 +1,4 @@
-import React, {
-	Component
-} from 'react'
+import React, {Component} from 'react'
 class Profile extends Component {
 
 	state = {
@@ -39,38 +37,7 @@ class Profile extends Component {
             <div className = "col-sm-8 mx-auto">
 			<h1 className = "text-center" > PROFILE </h1></div>
             <table className = "table col-md-6 mx-auto">
-            <tbody>
-			<tr>
-			<td> First Name < /td> <
-			td > {
-				this.state.first_name
-			} < /td> <
-			/tr> <
-			tr >
-			<
-			td > Last Name < /td> <
-			td > {
-				this.state.last_name
-			} < /td> <
-			/tr> <
-			tr >
-			<
-			td > Email < /td> <
-			td > {
-				this.state.email
-			} < /td> <
-			/tr> <
-			tr >
-			<
-			td > Photo < /td> <
-			td > {
-				this.state.Photo
-			} < /td> <
-			/tr> <
-			/tbody> <
-			/table> <
-			/div> <
-			/div>
+            <tbody><tr><td>First Name</td><td>{this.state.first_name}</td></tr><tr><td>Last Name</td><td>{this.state.last_name}</td></tr><tr><td>Email</td><td>{this.state.email}</td></tr><tr><td>Photo</td><td>{this.state.Photo}</td></tr></tbody></table></div></div>
 		)
 	}
 }
