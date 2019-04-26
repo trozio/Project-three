de2be3a70b830517588ddd49bea6dffd8708a5d9
helmi <helmitira@yahoo.com>
2019-04-26 17:28:01 -0400

    update ReviewPage

--- spotlight/src/components/Layouts/ReviewPage.js
+++ spotlight/src/components/Layouts/ReviewPage.js
@@ -1,5 +1,6 @@
-import React, { Component, Rating } from 'react';
-import { Form } from 'react-bootstrap';
+import React, { Component } from 'react';
+import { Form, Button } from 'react-bootstrap';
+// var Rating = require('react-rating');
 class ReviewPage extends Component {
     render() {
         return (
@@ -13,6 +14,16 @@ class ReviewPage extends Component {
     <Form.Label>Author</Form.Label>
     <Form.Control type="text" placeholder="John Doe" />
   </Form.Group>
+  <Form.Group controlId="exampleForm.ControlSelect1">
+    <Form.Label>Rating</Form.Label>
+            <Form.Control as="select">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+            <option>4</option>
+            <option>5</option>
+          </Form.Control>
+        </Form.Group>
   <Form.Group controlId="exampleForm.ControlSelect1">
     <Form.Label>Products Categories</Form.Label>
             <Form.Control as="select">
@@ -38,6 +49,9 @@ class ReviewPage extends Component {
           <Form.Control as="textarea" rows="3" />
         </Form.Group>
       </Form>
+      <Button variant="primary" type="submit">
+    Submit
+  </Button>
       </div>
         </div>
         </div>
