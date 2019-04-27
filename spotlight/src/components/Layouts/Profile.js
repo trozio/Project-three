cb306e14de50acc3052057d5275efe405b350b9a
helmi <helmitira@yahoo.com>
2019-04-27 11:58:35 -0400

    update profile

--- spotlight/src/components/Layouts/Profile.js
+++ spotlight/src/components/Layouts/Profile.js
@@ -31,13 +31,17 @@ class Profile extends Component {
 
 
 	render() {
-		return ( <
-			div className = "container" >
+		return ( 
+			<div className = "container" >
 			<div className = "jumbotron mt-5">
             <div className = "col-sm-8 mx-auto">
 			<h1 className = "text-center" > PROFILE </h1></div>
             <table className = "table col-md-6 mx-auto">
-            <tbody><tr><td>First Name</td><td>{this.state.first_name}</td></tr><tr><td>Last Name</td><td>{this.state.last_name}</td></tr><tr><td>Email</td><td>{this.state.email}</td></tr><tr><td>Photo</td><td>{this.state.Photo}</td></tr></tbody></table></div></div>
+			<tbody><tr><td>First Name</td><td value={this.state.first_name} ></td></tr>
+			<tr><td>Last Name</td><td value= {this.state.last_name}></td></tr>
+			<tr><td>Email</td><td value= {this.state.email}></td></tr>
+			<tr><td>Photo</td><td value= {this.state.Photo}></td></tr>
+			</tbody></table></div></div>
 		)
 	}
 }
