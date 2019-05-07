3706711814ce0de62c4202e66579711ba9d7b938
trozio <daniel.l.adelman@gmail.com>
2019-05-07 11:03:15 -0400

    Update signup.js

--- spotlight/src/components/Layouts/signup.js
+++ spotlight/src/components/Layouts/signup.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import { button, Form, FormGroup, label, Input } from 'reactstrap';
+import { button, Form, FormGroup, label, Input, Label, FormFeedback, FormText} from 'reactstrap';
 import { FacebookLoginButton } from 'react-social-login-buttons';
 
 
@@ -8,7 +8,13 @@ class signup extends Component {
     state ={
         name: '',
       email: '',
-      password:''
+      password:'',
+      nValidateTrue: false,
+      nValidateFalse: true,
+      eValidateTrue: false,
+      eValidateFalse: true,
+      pValidateTrue: false,
+      pValidateFalse: true
     }
 
     handleChange = (event) => {
@@ -21,6 +27,7 @@ class signup extends Component {
 
     handleSignup = (event) => {
       event.preventDefault();
+      if(this.state.nValidateTrue === true && this.state.eValidateTrue === true && this.state.pValidateTrue === true ){
       console.log(this.state.name);
       console.log(this.state.email);
       console.log(this.state.password);
@@ -58,6 +65,7 @@ class signup extends Component {
           }
         )
     }
+    }
 
   render () {
       return (
@@ -67,24 +75,28 @@ class signup extends Component {
         <Form className="signup-form">
           <h2 className="text-center">Welcome</h2>
           <FormGroup>
-             <label>Name</label>
-<Input value={this.state.name} onChange={(event) => this.setState({ name: event.target.value })} type="text" placeholder="John Doe"/>
+          <Label for="email">Email</Label>
+          <Input valid= {this.state.nValidateTrue} invalid={this.state.nValidateFalse} value={this.state.name} onChange={(event) => this.setState({ name: event.target.value, nValidateTrue: true, nValidateFalse: false })} type="name" placeholder="Name"/>
+          <FormFeedback tooltip>You cannot leave this field blank</FormFeedback>
+          </FormGroup>
+<FormGroup>
+<Label for="email">Email</Label>
+<Input valid= {this.state.eValidateTrue} invalid={this.state.eValidateFalse} value={this.state.email} onChange={(event) => this.setState({ email: event.target.value, eValidateTrue: true, eValidateFalse: false })} type="email" placeholder="Email"/>
+<FormFeedback tooltip>You cannot leave this field blank</FormFeedback>
 </FormGroup>
-          <FormGroup>
-             <label>Email</label>
-              <Input value={this.state.email} onChange={(event) => this.setState({ email: event.target.value })} type="text" placeholder="Email"/>
-          </FormGroup>
-          <FormGroup>
-             <label>Password</label>
-             <Input value={this.state.password} onChange={(event) => this.setState({ password: event.target.value })} type="password" placeholder="Password"/>
-          </FormGroup>
+
+<FormGroup>
+<Label for="password">Password</Label>
+<Input valid= {this.state.pValidateTrue} invalid={this.state.pValidateFalse} value={this.state.password} onChange={(event) => this.setState({ password: event.target.value, pValidateTrue: true, pValidateFalse: false })} type="password" placeholder="Password"/>
+<FormFeedback tooltip>You cannot leave this field blank</FormFeedback>
+</FormGroup>
           <button onClick={this.handleSignup} className="btn-lg btn-dark btn-block">Sign up</button>
           <div className="text-center pt-3">Or Continue with your social account</div>
           <FacebookLoginButton className="mt-3 mb-3"/>
           <div className="text-center">
             <a href="/login">login</a>
             <span className="p-2">|</span>
-            <a href="/forget-password">Forgot Password</a>
+            <a href="/login">Forgot Password</a>
           </div>
         </Form>
         </div>
